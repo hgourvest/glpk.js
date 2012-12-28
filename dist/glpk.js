afe640b7086015458a6ecb3cecb5bb15b75e3588
Henri Gourvest <hgourvest@gmail.com>
2012-12-28 21:37:41 +0100

    fix string formating

--- dist/glpk.js
+++ dist/glpk.js
@@ -1,4 +1,4 @@
-/*! glpk.js - v4.47.0 - 2012-12-26
+/*! glpk.js - v4.47.0 - 2012-12-28
 * https://github.com/hgourvest/glpk.js
 * Copyright (c) 2012 Henri Gourvest; Licensed GPLv2 */
 
@@ -140,6 +140,193 @@ function tolower(c){
     return c.toLowerCase();
 }
 
+
+function sprintf () {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Ash Searle (http://hexmen.com/blog/)
+    // + namespaced by: Michael White (http://getsprink.com)
+    // +    tweaked by: Jack
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Paulo Freitas
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Dj
+    // +   improved by: Allidylls
+    // *     example 1: sprintf("%01.2f", 123.1);
+    // *     returns 1: 123.10
+    // *     example 2: sprintf("[%10s]", 'monkey');
+    // *     returns 2: '[    monkey]'
+    // *     example 3: sprintf("[%'#10s]", 'monkey');
+    // *     returns 3: '[####monkey]'
+    // *     example 4: sprintf("%d", 123456789012345);
+    // *     returns 4: '123456789012345'
+    var regex = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuideEfFgG])/g;
+    var a = arguments,
+        i = 0,
+        format = a[i++];
+
+    // pad()
+    var pad = function (str, len, chr, leftJustify) {
+        if (!chr) {
+            chr = ' ';
+        }
+        var padding = (str.length >= len) ? '' : Array(1 + len - str.length >>> 0).join(chr);
+        return leftJustify ? str + padding : padding + str;
+    };
+
+    // justify()
+    var justify = function (value, prefix, leftJustify, minWidth, zeroPad, customPadChar) {
+        var diff = minWidth - value.length;
+        if (diff > 0) {
+            if (leftJustify || !zeroPad) {
+                value = pad(value, minWidth, customPadChar, leftJustify);
+            } else {
+                value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
+            }
+        }
+        return value;
+    };
+
+    // formatBaseX()
+    var formatBaseX = function (value, base, prefix, leftJustify, minWidth, precision, zeroPad) {
+        // Note: casts negative numbers to positive ones
+        var number = value >>> 0;
+        prefix = prefix && number && {
+            '2': '0b',
+            '8': '0',
+            '16': '0x'
+        }[base] || '';
+        value = prefix + pad(number.toString(base), precision || 0, '0', false);
+        return justify(value, prefix, leftJustify, minWidth, zeroPad);
+    };
+
+    // formatString()
+    var formatString = function (value, leftJustify, minWidth, precision, zeroPad, customPadChar) {
+        if (precision != null) {
+            value = value.slice(0, precision);
+        }
+        return justify(value, '', leftJustify, minWidth, zeroPad, customPadChar);
+    };
+
+    // doFormat()
+    var doFormat = function (substring, valueIndex, flags, minWidth, _, precision, type) {
+        var number;
+        var prefix;
+        var method;
+        var textTransform;
+        var value;
+
+        if (substring == '%%') {
+            return '%';
+        }
+
+        // parse flags
+        var leftJustify = false,
+            positivePrefix = '',
+            zeroPad = false,
+            prefixBaseX = false,
+            customPadChar = ' ';
+        var flagsl = flags.length;
+        for (var j = 0; flags && j < flagsl; j++) {
+            switch (flags.charAt(j)) {
+                case ' ':
+                    positivePrefix = ' ';
+                    break;
+                case '+':
+                    positivePrefix = '+';
+                    break;
+                case '-':
+                    leftJustify = true;
+                    break;
+                case "'":
+                    customPadChar = flags.charAt(j + 1);
+                    break;
+                case '0':
+                    zeroPad = true;
+                    break;
+                case '#':
+                    prefixBaseX = true;
+                    break;
+            }
+        }
+
+        // parameters may be null, undefined, empty-string or real valued
+        // we want to ignore null, undefined and empty-string values
+        if (!minWidth) {
+            minWidth = 0;
+        } else if (minWidth == '*') {
+            minWidth = +a[i++];
+        } else if (minWidth.charAt(0) == '*') {
+            minWidth = +a[minWidth.slice(1, -1)];
+        } else {
+            minWidth = +minWidth;
+        }
+
+        // Note: undocumented perl feature:
+        if (minWidth < 0) {
+            minWidth = -minWidth;
+            leftJustify = true;
+        }
+
+        if (!isFinite(minWidth)) {
+            throw new Error('sprintf: (minimum-)width must be finite');
+        }
+
+        if (!precision) {
+            precision = 'fFeE'.indexOf(type) > -1 ? 6 : (type == 'd') ? 0 : undefined;
+        } else if (precision == '*') {
+            precision = +a[i++];
+        } else if (precision.charAt(0) == '*') {
+            precision = +a[precision.slice(1, -1)];
+        } else {
+            precision = +precision;
+        }
+
+        // grab value using valueIndex if required?
+        value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
+
+        switch (type) {
+            case 's':
+                return formatString(String(value), leftJustify, minWidth, precision, zeroPad, customPadChar);
+            case 'c':
+                return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
+            case 'b':
+                return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'o':
+                return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'x':
+                return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'X':
+                return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad).toUpperCase();
+            case 'u':
+                return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'i':
+            case 'd':
+                number = +value || 0;
+                number = Math.round(number - number % 1); // Plain Math.round doesn't just truncate
+                prefix = number < 0 ? '-' : positivePrefix;
+                value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+                return justify(value, prefix, leftJustify, minWidth, zeroPad);
+            case 'e':
+            case 'E':
+            case 'f': // Should handle locales (as per setlocale)
+            case 'F':
+            case 'g':
+            case 'G':
+                number = +value;
+                prefix = number < 0 ? '-' : positivePrefix;
+                method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+                textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
+                value = prefix + Math.abs(number)[method](precision);
+                return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+            default:
+                return substring;
+        }
+    };
+
+    return format.replace(regex, doFormat);
+}
 
 
 /* glpapi.h */
@@ -8107,9 +8294,9 @@ function ios_driver(T){
         /* format the relative mip gap */
         temp = ios_relative_gap(T);
         if (temp == 0.0)
-            rel_gap = "  0.0%%";
+            rel_gap = "  0.0%";
         else if (temp < 0.001)
-            rel_gap = " < 0.1%%";
+            rel_gap = " < 0.1%";
         else if (temp <= 9.999)
             rel_gap = 100.0 * temp;
         else
@@ -24769,6 +24956,7 @@ function mpl_internal_printf_func(mpl, prt){
     /* this is auxiliary routine to work within domain scope */
     var entry;
     var fmt;
+    var from;
     var c;
     var value;
     /* evaluate format control string */
@@ -24782,7 +24970,7 @@ function mpl_internal_printf_func(mpl, prt){
     for (c = 0; c < fmt.length; c++)
     {  if (fmt[c] == '%')
     {  /* scan format specifier */
-        c++;
+        from = c++;
         if (fmt[c] == '%')
         {  mpl_internal_print_char(mpl, '%');
             continue;
@@ -24826,10 +25014,10 @@ function mpl_internal_printf_func(mpl, prt){
             {  var int_max = INT_MAX;
                 if (!(-int_max <= value && value <= +int_max))
                     mpl_internal_error(mpl, "cannot convert " + value + " to integer");
-                mpl_internal_print_text(mpl, String(Math.floor(value + 0.5)|0));
+                mpl_internal_print_text(mpl, sprintf(fmt.slice(from, c+1), Math.floor(value + 0.5)|0));
             }
             else
-                mpl_internal_print_text(mpl, String(value));
+                mpl_internal_print_text(mpl, sprintf(fmt.slice(from, c+1), value));
         }
         else if (fmt[c] == 's')
         {  /* the specifier requires symbolic value */
@@ -24853,7 +25041,7 @@ function mpl_internal_printf_func(mpl, prt){
                 default:
                     xassert(entry != entry);
             }
-            mpl_internal_print_text(mpl, String(value));
+            mpl_internal_print_text(mpl, sprintf(fmt.slice(from, c+1), value));
         }
         else
             mpl_internal_error(mpl, "format specifier missing or invalid");
